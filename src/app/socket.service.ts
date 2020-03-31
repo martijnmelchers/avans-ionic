e596b7595917c4fc4364e47d7e5d487005315cf9
Martijn <martijn@melchers.nu>
2020-03-31 12:12:38 +0200

    Add tunnel for 'on' socket

--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -1,19 +1,24 @@
-import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import {Injectable} from '@angular/core';
+import {environment} from 'src/environments/environment';
 import * as io from 'socket.io-client';
 import Socket = SocketIOClient.Socket;
+import Emitter = SocketIOClient.Emitter;
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class SocketService {
     private _socket: SocketIOClient.Socket;
 
     constructor() {
-      this._socket =  io.connect(`${environment.endpoints.api}`);
+        this._socket = io.connect(`${environment.endpoints.api}`);
     }
 
-    GetSocket(): Socket{
+    GetSocket(): Socket {
         return this._socket;
     }
+
+    on(event: string, fn: () => void): Emitter {
+        return this._socket.on(event, fn);
+    };
 }
